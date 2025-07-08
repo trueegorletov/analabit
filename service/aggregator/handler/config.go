f67eff622f47d555a8a48bdcae96411bb1b3f074
Analabit Deploy <analabit@analabit.ru>
2025-07-08 23:41:09 +0300

    fix(config): Standardize environment variables across all services

--- service/aggregator/handler/config.go
+++ service/aggregator/handler/config.go
@@ -4,11 +4,16 @@ package handler
 // Uses caarlos0/env struct tags for binding
 
 type config struct {
-	RabbitURL           string `env:"RABBITMQ_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
-	MinioEndpoint       string `env:"MINIO_ENDPOINT" envDefault:"minio:9000"`
-	MinioAccessKey      string `env:"MINIO_ACCESS_KEY_ID" envDefault:"minioadmin"`
-	MinioSecretKey      string `env:"MINIO_SECRET_ACCESS_KEY" envDefault:"minioadmin"`
-	MinioUseSSL         bool   `env:"MINIO_USE_SSL" envDefault:"false"`
-	MinioBucketName     string `env:"MINIO_BUCKET_NAME" envDefault:"analabit-results"`
-	PostgresConnStrings string `env:"POSTGRES_CONN_STRINGS"`
+	RabbitURL        string `env:"RABBITMQ_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
+	MinioEndpoint    string `env:"MINIO_ENDPOINT" envDefault:"minio:9000"`
+	MinioAccessKey   string `env:"MINIO_ACCESS_KEY_ID" envDefault:"minioadmin"`
+	MinioSecretKey   string `env:"MINIO_SECRET_ACCESS_KEY" envDefault:"minioadmin"`
+	MinioUseSSL      bool   `env:"MINIO_USE_SSL" envDefault:"false"`
+	MinioBucketName  string `env:"MINIO_BUCKET_NAME" envDefault:"analabit-results"`
+	DatabaseHost     string `env:"DATABASE_HOST" envDefault:"localhost"`
+	DatabasePort     int    `env:"DATABASE_PORT" envDefault:"5432"`
+	DatabaseUser     string `env:"DATABASE_USER" envDefault:"postgres"`
+	DatabaseDBName   string `env:"DATABASE_DBNAME" envDefault:"postgres"`
+	DatabasePassword string `env:"DATABASE_PASSWORD" envDefault:"postgres"`
+	DatabaseSSLMode  string `env:"DATABASE_SSLMODE" envDefault:"disable"`
 }
--- service/producer/handler/config.go
+++ service/producer/handler/config.go
@@ -12,10 +12,10 @@ type Config struct {
 	DrainStages            []int  `env:"DRAIN_SIM_STAGES" envSeparator:"," envDefault:"16,33,50,66"`
 	DrainIterations        int    `env:"DRAIN_SIM_ITERATIONS" envDefault:"100"`
 	MinioEndpoint          string `env:"MINIO_ENDPOINT" envDefault:"minio:9000"`
-	MinioAccessKey         string `env:"MINIO_ACCESS_KEY" envDefault:"minioadmin"`
-	MinioSecretKey         string `env:"MINIO_SECRET_KEY" envDefault:"minioadmin"`
+	MinioAccessKey         string `env:"MINIO_ACCESS_KEY_ID" envDefault:"minioadmin"`
+	MinioSecretKey         string `env:"MINIO_SECRET_ACCESS_KEY" envDefault:"minioadmin"`
 	MinioUseSSL            bool   `env:"MINIO_USE_SSL" envDefault:"false"`
-	RabbitURL              string `env:"RABBIT_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
+	RabbitURL              string `env:"RABBITMQ_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
 	MinioBucketName        string `env:"MINIO_BUCKET_NAME" envDefault:"analabit-results"`
 	SelfQueryPeriodMinutes int    `env:"SELF_QUERY_PERIOD_MINUTES" envDefault:"100"`
 }
