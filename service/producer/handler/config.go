de4d3c04f94aaaad48ece177a69865a06e0a79ac
Analabit Deploy <analabit@analabit.ru>
2025-07-08 23:12:35 +0300

    fix: revert environment variable prefixes in Config struct

--- service/producer/handler/config.go
+++ service/producer/handler/config.go
@@ -7,17 +7,17 @@ import (
 )
 
 type Config struct {
-	CacheDir               string `env:"ANALABIT_CACHE_DIR" envDefault:"./cache"`
-	CacheTTLMinutes        int    `env:"ANALABIT_CACHE_TTL_MINUTES" envDefault:"100"`
-	DrainStages            []int  `env:"ANALABIT_DRAIN_SIM_STAGES" envSeparator:"," envDefault:"16,33,50,66"`
-	DrainIterations        int    `env:"ANALABIT_DRAIN_SIM_ITERATIONS" envDefault:"100"`
-	MinioEndpoint          string `env:"ANALABIT_MINIO_ENDPOINT" envDefault:"minio:9000"`
-	MinioAccessKey         string `env:"ANALABIT_MINIO_ACCESS_KEY" envDefault:"minioadmin"`
-	MinioSecretKey         string `env:"ANALABIT_MINIO_SECRET_KEY" envDefault:"minioadmin"`
-	MinioUseSSL            bool   `env:"ANALABIT_MINIO_USE_SSL" envDefault:"false"`
-	RabbitURL              string `env:"ANALABIT_RABBIT_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
-	MinioBucketName        string `env:"ANALABIT_MINIO_BUCKET_NAME" envDefault:"analabit-results"`
-	SelfQueryPeriodMinutes int    `env:"ANALABIT_SELF_QUERY_PERIOD_MINUTES" envDefault:"100"`
+	CacheDir               string `env:"CACHE_DIR" envDefault:"./cache"`
+	CacheTTLMinutes        int    `env:"CACHE_TTL_MINUTES" envDefault:"100"`
+	DrainStages            []int  `env:"DRAIN_SIM_STAGES" envSeparator:"," envDefault:"16,33,50,66"`
+	DrainIterations        int    `env:"DRAIN_SIM_ITERATIONS" envDefault:"100"`
+	MinioEndpoint          string `env:"MINIO_ENDPOINT" envDefault:"minio:9000"`
+	MinioAccessKey         string `env:"MINIO_ACCESS_KEY" envDefault:"minioadmin"`
+	MinioSecretKey         string `env:"MINIO_SECRET_KEY" envDefault:"minioadmin"`
+	MinioUseSSL            bool   `env:"MINIO_USE_SSL" envDefault:"false"`
+	RabbitURL              string `env:"RABBIT_URL" envDefault:"amqp://guest:guest@rabbitmq:5672/"`
+	MinioBucketName        string `env:"MINIO_BUCKET_NAME" envDefault:"analabit-results"`
+	SelfQueryPeriodMinutes int    `env:"SELF_QUERY_PERIOD_MINUTES" envDefault:"100"`
 }
 
 var Cfg Config
